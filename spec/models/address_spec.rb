b67e105184796e8a9ccd003fe87e5223b112bf39
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2018-10-16 14:04:45 -0400

    updated address_spec.rb
    adjusted address_spec.rb  for additional coverage

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe Address, type: :model do
   let(:address) { FactoryBot.create :address_user_association }
 
-  it { should belong_to(:addressable) }
-
   context 'validations' do
     it { should validate_length_of(:street_address_1).is_at_most(50) }
     it { should validate_length_of(:street_address_2).is_at_most(50) }
@@ -12,11 +10,23 @@ RSpec.describe Address, type: :model do
     it { should validate_length_of(:state).is_at_most(50) }
     it { should validate_length_of(:zip_code).is_at_most(20) }
   end
+  
+  context 'associations' do
+    it { should belong_to(:addressable) }
+  end 
 
-  context 'valid object' do
-    it 'expects address to be valid' do
-      expect(address).to be_valid
-    end
+  it 'expects address to be valid' do
+    expect(address).to be_valid
   end
 
+  describe '#human_readable' do
+    it 'expects address to be human readable' do
+      format = "#{address.street_address_1} #{address.street_address_2}, #{address.city}, #{address.state} #{address.zip_code}"
+      expect(address.human_readable).to eq(format)
+    end
+
+    it 'expects a string value from the model' do
+      expect(address.human_readable.class).to eq(String) 
+    end
+  end  
 end
