85b58fcb4103d6a7c60ccb16fdb24dd7a78ede47
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2018-12-14 15:06:38 -0500

    Adjusted search
    Changed search to only search faculty profile. Doesn't include separate search for colleges or departments.

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -3,15 +3,15 @@ class DirectoryController < ApplicationController
 
   def index
     if params[:search].nil?
-      @faculty_profiles = Faculty.includes(:phones, :addresses)
+      @results = Faculty.includes(:phones, :addresses)
                                  .where(visible: true, status: 'enabled')
                                  .order(:last_name, :first_name)
     else
       clean_term = params[:search].gsub(%r{\{|\}|\[|\]|\\|\/|\^|\~|\:|\!|\"|\'}, '')
       @search_term = Sanitize.fragment clean_term
-      @faculty_profiles = Elasticsearch::Model.search(
+      @results = Elasticsearch::Model.search(
         @search_term,
-        [College, Department, Faculty],
+        [Faculty],
         size: 1000
       ).results
     end  
