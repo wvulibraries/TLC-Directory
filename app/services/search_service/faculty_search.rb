c5c859504ec7970ed21d0abbdd4797a338ab7b76
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-01-10 13:01:54 -0500

    refactor faculty search

--- app/services/search_service/faculty_search.rb
+++ app/services/search_service/faculty_search.rb
@@ -19,71 +19,35 @@ module SearchService
                 @search_term.present? || @college_id.present? || @dept_id.present?
             end
 
-            def setup_full_query
-                @search_definition = {}
-                @search_definition[:query]  = { bool:{
-                    should:{
-                    },
-                    filter:{
-                    }
-                }}
-            end
-
-            def setup_basic_query
-                @search_definition = {}
-                @search_definition[:query]  = { bool:{
-                    should:{
-                    }
-                }}
-            end
-
-            def setup_filtered_query
-                @search_definition = {}
-                @search_definition[:query]  = { bool:{
-                    filter:{
-                    }
-                }}
-            end
-
             def build_query
+                @search_definition = {
+                    :query => {
+                        :bool => {
+                        }
+                    }
+                }
+
                 if @search_term.present?
                     sanitized_search = Sanitize.fragment @search_term.gsub(%r{\{|\}|\[|\]|\\|\/|\^|\~|\:|\!|\"|\'}, '')
-                    query_array = []
-                    query_array.push( {:query_string => {:query => sanitized_search}} )
-                end
-
+                    @search_definition[:query][:bool][:should] = [{:query_string => {:query => sanitized_search}}]
+                end   
+                             
                 # add optional filters
                 term_array = []
                 term_array.push( {:term => {:college_id => @college_id}} ) if @college_id.present?
                 term_array.push( {:term => {:department_id => @dept_id}} ) if @dept_id.present?
-
-                setup_full_query
-                @search_definition[:query][:bool][:should] = query_array
-                @search_definition[:query][:bool][:filter] = term_array 
-                
-                # if query_array.present? && term_array.present?
-                #    setup_full_query
-                #    @search_definition[:query][:bool][:should] = query_array
-                #    @search_definition[:query][:bool][:filter] = term_array
-                # elsif query_array.present?
-                #    setup_basic_query
-                #    @search_definition[:query][:bool][:should] = query_array
-                # elsif term_array.present?
-                #   setup_filtered_query
-                #   @search_definition[:query][:bool][:filter] = term_array
-                # end
-            end
-
-            def full_query
-                @results = Faculty.includes(:college, :department)
-                                .where(visible: true, status: 'enabled')
-                                .order(:last_name, :first_name)
+                @search_definition[:query][:bool][:filter] = term_array if term_array.count > 0
             end
 
             def execute_query
-                 puts @search_definition.inspect
-                 @results = Faculty.search(@search_definition, size: 1000).records
+                 Faculty.search(@search_definition, size: 1000).records
                     .includes(:college, :department)
+                    .where(visible: true, status: 'enabled')
+                    .order(:last_name, :first_name)
+            end            
+
+            def full_query
+                Faculty.includes(:college, :department)
                     .where(visible: true, status: 'enabled')
                     .order(:last_name, :first_name)
             end
