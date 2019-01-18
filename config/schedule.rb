12d159411fb054b45551740a3af8368a2fc20081
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-01-18 12:39:09 -0500

    updated secrets and schedule.rb
    discovered that secrete was the same as another app so I reset it in the docker-compose. Also removed the env section of the schedule.rb as it was unneeded in this application.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,3 @@
-# set environmentals
-env :PATH, ENV['PATH']
-env :GEM_HOME, ENV['GEM_HOME']
-env :BUNDLE_APP_CONFIG, ENV['BUNDLE_APP_CONFIG']
-env :RUBY_MAJOR, ENV['RUBY_MAJOR']
-env :RUBYGEMS_VERSION, ENV['RUBYGEMS_VERSION']
-env :BUNDLE_BIN, ENV['BUNDLE_BIN']
-env :BUNDLE_PATH, ENV['BUNDLE_PATH']
-env :RUBY_VERSION, ENV['RUBY_VERSION']
-env :BUNDLER_VERSION, ENV['BUNDLER_VERSION']
-
-# needs to set environments for MYSQL
-env :MYSQL_PASSWORD, ENV['MYSQL_PASSWORD']
-env :MYSQL_HOST, ENV['MYSQL_HOST']
-env :MYSQL_PORT, ENV['MYSQL_PORT']
 
 # set logs and environment
 set :output, {:standard => "/home/tlcdirectory/log/cron.log", :error => "/home/tlcdirectory/log/cron_error.log"}
