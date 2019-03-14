1f358191b6aa17f319fc2a388af70d859032216b
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-03-14 10:46:26 -0400

    adjusted faculty_params
    faculty_params was very long. pulled and grouped params into smaller functions that we then included into faculty_params.

--- app/controllers/admin/faculties_controller.rb
+++ app/controllers/admin/faculties_controller.rb
@@ -98,20 +98,30 @@ class Admin::FacultiesController < ApplicationController
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
+  def name_attrs
+    [ :prefix, :suffix, :first_name, :middle_name, :last_name, :preferred_name]
+  end
+
+  def image_attrs 
+    [ :image, :image_cache, :remove_image ]
+  end
+
+  def resume_attrs
+    [ :resume, :resume_cache, :remove_resume ]
+  end
+
+  def contact_attrs
+    [ 
+      addresses_attributes: %i[id street_address_1 street_address_2 city state zip_code _destroy],
+      phones_attributes: %i[id number number_types _destroy], 
+    ]
+  end
+
   def faculty_params
     params.require(:faculty)
-          .permit(:prefix,
-                  :suffix,
-                  :first_name,
-                  :middle_name,
-                  :last_name,
-                  :preferred_name,
-                  :image,
-                  :image_cache,
-                  :remove_image, 
-                  :resume,
-                  :resume_cache,
-                  :remove_resume,                   
+          .permit(name_attrs,
+                  image_attrs,
+                  resume_attrs,                   
                   :wvu_username,
                   :email,
                   :title,
@@ -120,13 +130,13 @@ class Admin::FacultiesController < ApplicationController
                   :role,
                   :visible,
                   :research_interests,
+                  :teaching_interests,
                   :college_id, 
                   :department_id,     
-                  addresses_attributes: %i[id street_address_1 street_address_2 city state zip_code _destroy],
+                  contact_attrs,
                   awards_attributes: %i[id starting_year ending_year name organization description _destroy],
-                  phones_attributes: %i[id number number_types _destroy],
                   publications_attributes: %i[id starting_year ending_year author title status publisher pagenum issue volume url description _destroy],
-                  websites_attributes: %i[id url _destroy]           
+                  websites_attributes: %i[id url _destroy]          
                 )
     end
 
