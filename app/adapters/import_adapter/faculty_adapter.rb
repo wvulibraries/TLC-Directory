0aacb363a8b217b26e9b6395d5e45555c8f9e329
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-03-18 13:44:03 -0400

    refactored faculty_adapter.rb, Updated Gemfile.lock and added tests.

--- app/adapters/import_adapter/faculty_adapter.rb
+++ app/adapters/import_adapter/faculty_adapter.rb
@@ -10,27 +10,45 @@ module ImportAdapter
             hash[:perferred_name] = row[:pfname] unless row[:pfname].nil? 
             hash[:websites] = [Website.find_or_create_by(url: row[:website])] unless row[:website].nil?            
             hash[:phones] = build_phone_array(row)
-            @faculty.assign_attributes(hash)
+            @faculty.attributes = hash
         end
+
+        def create_phone(number, type)
+            Phone.find_or_create_by(number: number, number_types: type) if number.present? && type.present?
+        end
+
+        def create_number(area_code, prefix, line_number, extension)
+            if area_code.present? && prefix.present? && line_number.present?
+                number = area_code + '.' + prefix + '.' + line_number
+                number = number + 'Ext. ' + extension if extension.present? 
+                return number      
+            end
+        end
+
+        def create_office_phone(row)
+            number = create_number(row[:ophone1], row[:ophone2], row[:ophone3], row[:ophone4])
+            create_phone(number, 'office')
+        end
+
+        def create_department_phone(row)
+            number = create_number(row[:dphone1],row[:dphone2], row[:dphone3], row[:dphone4])
+            create_phone(number, 'department')
+        end      
+
+        def create_fax_phone(row)
+            number = create_number(row[:fax1], row[:fax2], row[:fax3], row[:fax4])
+            create_phone(number, 'department')
+        end 
 
         def build_phone_array(row)
             phone_array = []
-            phone = create_phone(row[:ophone1], row[:ophone2], row[:ophone3], row[:ophone4], 'office')
+            phone = create_office_phone(row)
             phone_array << phone unless phone.nil?
-            phone = create_phone(row[:dphone1],row[:dphone2], row[:dphone3], row[:dphone4], 'department')
+            phone = create_department_phone(row)
             phone_array << phone unless phone.nil?
-            phone = create_phone(row[:fax1],row[:fax2], row[:fax3], row[:fax4], 'fax')
+            phone = create_fax_phone(row)
             phone_array << phone unless phone.nil?
             return phone_array
-        end
-
-        def create_phone(area_code, prefix, line_number, extension, type)
-            if area_code.present? && prefix.present? && line_number.present? && type.present?
-                number = area_code + '.' + prefix + '.' + line_number
-                # Add optional extension
-                number = number + 'Ext. ' + extension if extension.present?
-                Phone.find_or_create_by(number: number, number_types: type)
-            end
         end
 
     end
---
+++ spec/adaptors/import_adapter/faculty_adapter_spec.rb
@@ -0,0 +1,35 @@
+require 'csv'
+require 'rails_helper'
+
+RSpec.describe ImportAdapter::FacultyAdapter do
+  describe 'methods included in module' do
+    let(:faculty) { FactoryBot.attributes_for :faculty }
+    let(:college) { FactoryBot.attributes_for :college }
+    let(:dept) { FactoryBot.attributes_for :department }
+
+    let(:header) { "First Name" + ',' + "Middle Name" + ',' +  "Last Name" + ',' + "Email"  + ',' + "College (Most Recent)" + ',' + "Section (Department of Medicine Only) (Most Recent)" + ',' + "Unit (Most Recent)" + ',' + "USERNAME" + ',' + "ophone1" + ',' + "ophone2" + ',' +  "ophone3" + ',' + "ophone4" }
+    let(:row2) { faculty[:first_name] + ',' + faculty[:middle_name] + ',' + faculty[:last_name] + ',' + faculty[:email] + ',' + college[:name] + ',,' + dept[:name] + ',' + faculty[:wvu_username] + ',' + '999' + ',' + '999' + ',' + '9999' + ',' + '999'}
+    let(:rows) { [header, row2] }    
+
+    let(:file_path) { "tmp/PCI.csv" }
+    let!(:csv) do
+      CSV.open(file_path, "w") do |csv|
+        rows.each do |row|
+          csv << row.split(",")
+        end
+      end
+    end
+
+    after(:each) { File.delete(file_path) }
+
+    context "with complete csv file" do
+      it 'perform import with valid params' do
+        adaptor = ImportAdapter::FacultyAdapter.new({:filename => file_path})
+        adaptor.import
+        # verify count
+        expect(adaptor.import_count).to eql(1)     
+      end
+    end
+
+  end
+end
