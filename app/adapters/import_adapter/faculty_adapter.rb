22aaebc2d4feb8093d8891184163faa812d1c529
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-04-05 14:23:01 -0400

    updated faculty_adapter.rb

--- app/adapters/import_adapter/faculty_adapter.rb
+++ app/adapters/import_adapter/faculty_adapter.rb
@@ -7,9 +7,9 @@ module ImportAdapter
     # placeholder for adding additional fields for the faculty model
     def add_optional_items(row)
       hash = {}
-      hash[:biography] = row[:bio] unless row[:bio].nil?
-      hash[:perferred_name] = row[:pfname] unless row[:pfname].nil?
-      hash[:websites] = create_website(row[:website])
+      hash[:biography] = row[:bio] unless row[:bio].blank?
+      hash[:perferred_name] = row[:pfname] unless row[:pfname].blank?
+      hash[:websites] = create_website(row[:website]) unless row[:website].blank?
       hash[:phones] = build_phone_array(row)
       @faculty.attributes = hash
     end
@@ -26,60 +26,13 @@ module ImportAdapter
       Phone.find_or_create_by(number: number, number_types: type)
     end
 
-    # def create_number(hash)
-    #   return unless valid_number_hash?(hash)
-
-    #   number = hash[:area_code] + '.' + hash[:prefix] + '.' + hash[:line_number]
-    #   return number unless hash[:extension].present?
-
-    #   # add extension and return
-    #   number + 'Ext. ' + hash[:extension]
-    # end
-
-    # def valid_number_hash?(hash)
-    #   hash[:area_code].present? && hash[:prefix].present? && hash[:line_number].present?
-    # end
-
-    # def create_office_phone(row)
-    #   hash = {}
-    #   hash[:area_code] = row[:ophone1]
-    #   hash[:prefix] = row[:ophone2]
-    #   hash[:line_number] = row[:ophone3]
-    #   hash[:extension] = row[:ophone4]
-
-    #   number = create_number(hash)
-    #   create_phone(number, 'office')
-    # end
-
-    # def create_department_phone(row)
-    #   hash = {}
-    #   hash[:area_code] = row[:dphone1]
-    #   hash[:prefix] = row[:dphone2]
-    #   hash[:line_number] = row[:dphone3]
-    #   hash[:extension] = row[:dphone4]
-
-    #   number = create_number(hash)
-    #   create_phone(number, 'department')
-    # end
-
-    # def create_fax_phone(row)
-    #   hash = {}
-    #   hash[:area_code] = row[:fax1]
-    #   hash[:prefix] = row[:fax2]
-    #   hash[:line_number] = row[:fax3]
-    #   hash[:extension] = row[:fax4]
-
-    #   number = create_number(hash)
-    #   create_phone(number, 'fax')
-    # end
-
     def create_number(*args)
       area_code, prefix, line_number, extension, type = *args
 
       return if area_code.blank? || prefix.blank? || line_number.blank? || type.blank?
 
       number = [area_code, prefix, line_number].join('.')
-      number << "Ext. {extension}" unless extension.blank?
+      number << 'Ext. {extension}' unless extension.blank?
 
 
       create_phone(number, type)
