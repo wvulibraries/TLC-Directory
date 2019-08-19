6a11643c2fc5d402b641c368f090bb0ec0a20a2f
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-08-19 17:10:10 -0400

    Updated SCSS and Admin Views
    Adjusted and added more scss. Worked on views in the admin section.

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ service cron start
 whenever --update-crontab
 
 # remove PID and start the server
-file="/home/tlcdirectory/tmp/pids/server.pid"
+file="./tmp/pids/server.pid"
 [ -f $file ] && rm $file
 
 bin/rails s -p 3000 -b '0.0.0.0'
